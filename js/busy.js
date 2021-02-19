decb5d4075d0c288336d52fccd4e9544bc993690
finley <FinleyLu@trevi.cc>
2021-02-19 18:22:48 +0800

    serious JS blocking!!

--- js/busy.js
+++ js/busy.js
@@ -1,6 +1,6 @@
 (function () {
   console.log("hello world");
-  for (let index = 0; index < 1000000000; index++) {
+  for (let index = 0; index < 10000000000; index++) {
     /** busy loop */
   }
 })();
